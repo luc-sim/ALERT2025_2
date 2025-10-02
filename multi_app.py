bb4e3ca08613c0ac69f3f918994e6fd9d6950f70
luc-sim <58327465+luc-sim@users.noreply.github.com>
2025-10-02 11:42:59 +0200

    Update multi_app.py

--- multi_app.py
+++ multi_app.py
@@ -7,8 +7,7 @@ N = st.slider("Number of surfaces", min_value=1, max_value=10, value=1, step=1)
 fu = st.slider("Ultimate force", min_value=0.0, max_value=300.0, value=100.0, step=10.0)
 h = st.slider("Hardening factor",min_value=1.0, max_value=10000.0, value=100.0, step=1.0)
 b = st.slider("Hardening exponent",min_value=0.1, max_value=5.0, value=1.0, step=0.1)
-if st.button("Calculate"):
-
+if st.button("Monotonic response simulation"):
     # Call plotting function
     fig = mono(fu,h,b,int(N))
     st.pyplot(fig)   # <-- display in Streamlit
