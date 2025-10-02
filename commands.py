4a721fdfb7193c2914cdb42c537d500a74e078e8
luc-sim <58327465+luc-sim@users.noreply.github.com>
2025-10-02 11:56:29 +0200

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 12 15:01:41 2025
-
 @author: lsimonin
-
-newp.plotD([],tmd1+tmd2d+tmd3d+tmd4d+tmd5d+tmd6d,['k','b','g','r','c','m'],[500,-0,500,0,0.25,-0.00,0.04,-0.06],['1','2','3','4','5','6'])
-newp.plotD([],tmd1+tmd2l+tmd3l+tmd4l+tmd5l+tmd6l,['k','b','g','r','c','m'],[500,-0,500,0,0.25,-0.00,0.04,-0.06],['1','2','3','4','5','6'])
-newp.plotU([],tmu1+tmu2d+tmu3d+tmu4d+tmu5d+tmu6d,['k','b','g','r','c','m'],[400,-0,250,0,0.05,-0.00,0.04,-0.06],['1','2','3','4','5','6'],yt1=4,yt2=4)
-
-newp.plotD([],tmd1+tmd1bis,['k','k--'],[500,-0,500,0,0.25,-0.00,0.025,-0.08],['     m=0.7\n'+'non-linear elastic','     m=0\n'+'linear elastic'])
-newp.plotU([],tmu1+tmu1bis,['k','k--'],[500,-0,500,0,0.05,-0.00,0.025,-0.08],['     m=0.7\n'+'non-linear elastic','     m=0\n'+'linear elastic'])
-
 """
 
 from HyperDrive import Commands as H
@@ -55,8 +45,27 @@ def mono(fu,h,b,NN):
     H.general_inc([1,0,0,0,0,0,0,1,0.0,0.03,1,10,prec])
     dt = H.returnrec()
     # w,dt = TC.drained_test(pt,qt,vt,eqd,prec,params,bug)
-    fig = newp.plotA([],dt,['r-.'],[300,-0,200,0,0.05,-0.00,0.04,-0.06],[''],lab=['',''],ncl=1,lw=1.75,ttl='Drained test')
+    fig = newp.plotA([],dt,['r-.'],[300,-0,200,0,0.05,-0.00,0.04,-0.06],[''],lab=['',''],ncl=1,lw=1.75,ttl='Monotonic test')
     return fig
 
-
 mono(fu,h,b,NN)
+
+def cyclic(fu,h,b,NN):
+    pt,qt=0.01,0 # Initial stress values
+    vt = 2
+    params = [NN,K,G,fu,h,b]
+    H.const(params)
+    H.start()
+    H.init_stress([pt,qt])
+    for i in range(5):
+        H.general_inc([1,0,0,1,0,0,0,0,0.0,0.01,1,10,prec])
+        H.general_inc([1,0,0,1,0,0,0,0,0.0,200,1,10,prec])
+        H.general_inc([1,0,0,1,0,0,0,0,0.0,-200,1,10,prec])
+        H.general_inc([1,0,0,1,0,0,0,0,0.0,-200,1,10,prec])
+        H.general_inc([1,0,0,1,0,0,0,0,0.0,200,1,10,prec])
+    H.general_inc([1,0,0,1,0,0,0,0,0.0,300,1,10,prec])
+
+    dt = H.returnrec()
+    # w,dt = TC.drained_test(pt,qt,vt,eqd,prec,params,bug)
+    fig = newp.plotA([],dt,['r-.'],[250,-250,200,0,0.05,-0.05,0.04,-0.06],[''],lab=['',''],ncl=1,lw=1.75,ttl='Cyclic test')
+    return fig
