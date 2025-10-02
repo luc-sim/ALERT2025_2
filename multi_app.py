1927e139e025f5021d60acbbe8049c59a6786951
luc-sim <58327465+luc-sim@users.noreply.github.com>
2025-10-02 11:40:00 +0200

    Update multi_app.py

--- multi_app.py
+++ multi_app.py
@@ -3,12 +3,12 @@ from commands import mono
 
 st.title("Monotonic loading with multi-surface model")
 
-N = st.slider("Number of surfaces", 1, 10, 1)
-fu = st.slider("Ultimate force", 0, 300, 10)
-h = st.slider("Hardening factor",1,10000,1)
-b = st.slider("Hardening exponent",0.1,5,0.1)
+N = st.slider("Number of surfaces", min_value=1, max_value=10, value=1, step=1)
+fu = st.slider("Ultimate force", min_value=0.0, max_value=300.0, value=100.0, step=10.0)
+h = st.slider("Hardening factor",min_value=1.0, max_value=10000.0, value=100.0, step=1.0)
+b = st.slider("Hardening exponent",min_value=0.1, max_value=5.0, value=1.0, step=0.1)
 if st.button("Calculate"):
 
     # Call plotting function
-    fig = mono(fu,h,b,NN)
+    fig = mono(fu,h,b,int(NN))
     st.pyplot(fig)   # <-- display in Streamlit
