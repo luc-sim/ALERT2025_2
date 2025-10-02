97c77e4784819339c44e1963508b482ce107c9e5
luc-sim <58327465+luc-sim@users.noreply.github.com>
2025-10-02 11:35:47 +0200

    Update multi_app.py

--- multi_app.py
+++ multi_app.py
@@ -6,7 +6,7 @@ st.title("Monotonic loading with multi-surface model")
 N = st.slider("Number of surfaces", 1, 10, 1)
 fu = st.slider("Ultimate force", 0, 300, 10)
 h = st.slider("Hardening factor",1,10000,1)
-b = st.slide("Hardening exponent",0.1,5,0.1)
+b = st.slider("Hardening exponent",0.1,5,0.1)
 if st.button("Calculate"):
 
     # Call plotting function
